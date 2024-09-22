cbdd46343183eb58963ffc88c1fc3a4336e2d821
FaelSantoss <silva.faelm@gmail.com>
2024-09-22 10:03:06 -0300

    created class Cliente

---
+++ src/models/Cliente.ts
@@ -0,0 +1,58 @@
+import CPF from "./Cpf"
+import Pet from "./Pet"
+import Produto from "./Produto"
+import RG from "./Rg"
+import Servico from "./Servico"
+import Telefone from "./Telefone"
+
+export default class Cliente {
+    public nome: string
+    public nomeSocial: string
+    private cpf: CPF
+    private rgs: Array<RG>
+    private dataCadastro: Date
+    private telefones: Array<Telefone>
+    private produtosConsumidos: Array<Produto>
+    private servicosConsumidos: Array<Servico>
+    private pets: Array<Pet>
+    constructor(nome: string, nomeSocial: string, cpf: CPF) {
+        this.nome = nome
+        this.nomeSocial = nomeSocial
+        this.cpf = cpf
+        this.rgs = []
+        this.dataCadastro = new Date()
+        this.telefones = []
+        this.produtosConsumidos = []
+        this.servicosConsumidos = []
+        this.pets = []
+    }
+    public get getCpf(): CPF {
+        return this.cpf
+    }
+    public get getRgs(): Array<RG> {
+        return this.rgs
+    }
+    public get getDataCadastro(): Date {
+        return this.dataCadastro
+    }
+    public get getTelefones(): Array<Telefone> {
+        return this.telefones
+    }
+    public get getProdutosConsumidos(): Array<Produto> {
+        return this.produtosConsumidos
+    }
+    public get getServicosConsumidos(): Array<Servico> {
+        return this.servicosConsumidos
+    }
+    public get getPets(): Array<Pet>{
+        return this.pets
+    }
+
+    public adicionarPet(pet: Pet): void {
+        this.pets.push(pet)
+    }
+
+    public removerPet(pet: Pet): void {
+        this.pets = this.pets.filter(p => p !== pet)
+    }
+}
