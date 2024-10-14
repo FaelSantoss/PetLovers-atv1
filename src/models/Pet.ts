95096dc5a20de2aa5a7fdcdaccc28d9089ace124
FaelSantoss <silva.faelm@gmail.com>
2024-10-14 10:32:57 -0300

    add methods to the Pet entity

--- src/models/Pet.ts
+++ src/models/Pet.ts
@@ -1,9 +1,15 @@
+import Compra from "./Compra"
+import Produto from "./Produto"
+import Servico from "./Servico"
+
 export default class Pet {
     private nome: string
     private nomeDono: string
     private tipo: string
     private raca: string
     private genero: string
+    private produtosConsumidos: Array<Compra>
+    private servicosConsumidos: Array<Compra>
 
     constructor(nome: string, nomeDono: string, raca: string, genero: string, tipo: string) {
         this.nome = nome
@@ -11,6 +17,8 @@ export default class Pet {
         this.raca = raca
         this.genero = genero
         this.tipo = tipo
+        this.produtosConsumidos = []
+        this.servicosConsumidos = []
     }
 
     public get getNome(){return this.nome}
@@ -18,4 +26,19 @@ export default class Pet {
     public get getRaca(){return this.raca}
     public get getGenero(){return this.genero}
     public get getTipo(){return this.tipo}
+
+    public get getProdutosConsumidos(): Array<Compra> {
+        return this.produtosConsumidos
+    }
+    public get getServicosConsumidos(): Array<Compra> {
+        return this.servicosConsumidos
+    }
+
+    public adicionarProdutoConsumido(compra: Compra): void {
+        this.produtosConsumidos.push(compra);
+    }
+    
+    public adicionarServicoConsumido(compra: Compra): void {
+        this.servicosConsumidos.push(compra);
+    }
 }
